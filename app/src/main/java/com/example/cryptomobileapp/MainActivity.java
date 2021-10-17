112e252f862fec49d6f7e5b0f7b5f4e8f6ab33b1
Ayesh Hallawaarachchi <ayeshsandeepa7@gmail.com>
2021-10-17 21:47:59 +0530

    Buttons to redirect to get codes and login

--- app/src/main/java/com/example/cryptomobileapp/MainActivity.java
+++ app/src/main/java/com/example/cryptomobileapp/MainActivity.java
@@ -15,12 +15,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button voteButton=(Button) findViewById(R.id.voteButton);
+        Button registerButton=(Button) findViewById(R.id.registerButton);
+        Button loginButton=(Button) findViewById(R.id.loginButton);
 
         voteButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(v.getContext(), VotingActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(v.getContext(), RequestSecretActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(v.getContext(), LoginActivity.class);
                 v.getContext().startActivity(intent);
             }
         });
