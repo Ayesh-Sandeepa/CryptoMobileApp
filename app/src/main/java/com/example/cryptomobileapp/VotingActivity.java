cd09f46faabf7ae9708e302f11c7599bf4d0524f
GayanJayakody <gayanjayakody13@gmail.com>
2021-10-25 16:50:29 +0530

    Update VotingActivity.java

--- app/src/main/java/com/example/cryptomobileapp/VotingActivity.java
+++ app/src/main/java/com/example/cryptomobileapp/VotingActivity.java
@@ -1,18 +1,36 @@
 package com.example.cryptomobileapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
 
 public class VotingActivity extends AppCompatActivity {
 
@@ -23,6 +41,7 @@ public class VotingActivity extends AppCompatActivity {
 
         TextView elec=findViewById(R.id.election);
         TextView ballot=findViewById(R.id.ballotID);
+        TextView pk=findViewById(R.id.textViewPK);
 
         try{
             JSONObject obj=new JSONObject(loadJSONFromAsset());
@@ -46,11 +65,12 @@ public class VotingActivity extends AppCompatActivity {
         partyRecyclerView.setAdapter(partyAdapter);
         numbersRecyclerView.setAdapter(numbersAdapter);
 
+        pk.setText(getPublicKey());
+
     }
 
     public String loadJSONFromAsset(){
         String json=null;
-
         try{
             InputStream is=getAssets().open("ballotPaper.json");
             int size=is.available();
@@ -62,7 +82,30 @@ public class VotingActivity extends AppCompatActivity {
             ex.printStackTrace();
             return null;
         }
-
         return json;
+    }
+
+    public String getPublicKey(){
+        try {
+            InputStream is = getResources().openRawResource(R.raw.certificate);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            String key =new String(buffer,"UTF-8");
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            Certificate crt = cf.generateCertificate(new ByteArrayInputStream(buffer));
+            PublicKey publickey = crt.getPublicKey();
+            RSAPublicKey rsaPub  = (RSAPublicKey)(publickey);
+            BigInteger modulus = rsaPub.getModulus();
+            BigInteger exponent = rsaPub.getPublicExponent();
+            String mod = exponent.toString();
+            return mod;
+        }catch (IOException ex){
+            ex.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+        return "hgj";
     }
 }
